4e1dd4d3a5a7c6df13935214d412d27d56ca7fa6
Ankur Rana <113747952+Rana8535@users.noreply.github.com>
2024-07-26 12:18:29 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -25,7 +25,7 @@ const Footer = () => {
         </VStack>
 
         <VStack>
-          <Text>Ankur,Navneet</Text>
+          <Text>Ankur Rana</Text>
         </VStack>
       </Stack>
     </Box>
